0879f18ad46864bb16a0695f4ca621a7d93fed84
NoBugInMyCode <2940009621@qq.com>
2025-05-16 18:21:16 -0700

    update

--- backend/share_manager.py
+++ backend/share_manager.py
@@ -1,3 +1,4 @@
+import json
 from typing import Dict, List
 
 from backend.error import ErrorCode
@@ -13,3 +14,23 @@ class ShareManager:
         if node in self.share_list[from_username]:
             return ErrorCode.ALREADY_SHARED
         self.share_list[from_username].append(node)
+
+    def to_dict(self):
+        return {
+            from_user: [node.to_dict() for node in node_list]
+            for from_user, node_list in self.share_list.items()
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        sm = cls()
+        for from_user, node_dict_list in data.items():
+            sm.share_list[from_user] = [Node.from_dict(nd) for nd in node_dict_list]
+        return sm
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=2)
+
+    @classmethod
+    def from_json(cls, json_str):
+        return cls.from_dict(json.loads(json_str))
